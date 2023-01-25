6c1b478d85bb5ab9c2db4b29039dcdadadfbff84
meh <meh@IT5>
2023-01-25 15:47:56 +0800

    Add project files.

---
+++ DZ-5m-33l/Program.cs
@@ -0,0 +1,121 @@
+﻿
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Создание объектов классов и подписка на событие
+        Shop shop = new Shop();
+        Customer customer = new Customer();
+        shop.product.CollectionChanged += customer.OnItemChanged;
+
+        ///////////
+        ///Цикл выполнения
+        bool start = true;
+        do
+        {
+            Console.WriteLine("По нажатии клавиши A добавляется новый товар в магазин");
+            Console.WriteLine("По нажатии клавиши D удаление товара");
+            Console.WriteLine("X выход из программы");
+
+            string action = Console.ReadLine().ToUpper();
+            switch (action)
+            {
+                case "A":
+                    shop.Add();
+                    break;
+                case "D":
+                    //Надеюсь пользователь не дурак)
+                    Console.WriteLine("Введите индекс");
+                    shop.Remove(Int32.Parse(Console.ReadLine()));                    
+                    break;
+                case "X":
+                    start = false;
+                    break;
+                default:
+                    Console.WriteLine("Ты чё дурак?");
+                    break;
+            }
+            foreach (var item in shop.product)
+            {
+                Shop.Item.Print(item);
+            }
+
+
+
+        } while (start);
+
+        
+
+    }
+
+   
+
+
+    //Класс магазин
+    public class Shop
+    {
+        //Коллекция товаров
+       public ObservableCollection<Item> product = new ObservableCollection<Item>();
+        //Счётчик для индексов
+        int counter = 1;
+
+        //Класс Товар
+        public class Item
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public static void Print(Item a)
+            {
+                Console.WriteLine(a.Name);
+                Console.WriteLine(a.Id);
+
+            }
+        }
+
+        //Методы класса
+        public void Add()
+        {
+            product.Add(new Item { Id = counter, Name = $"Товар от {DateTime.Now}" });
+            counter++;
+        }
+        public void Remove(int Id)
+        {
+            foreach (var item in product)
+            {
+                if (item.Id == Id)
+                {
+                    product.Remove(item);
+                    counter--;
+                    break;
+                }
+            }
+        }
+    }
+
+    //Класс покупатель
+    public class Customer
+    {
+        public void OnItemChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        {
+
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    Console.WriteLine("Добавлен элемент");
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                    Console.WriteLine("Удалён элемент");
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+
+    
+}
---
+++ Librarian/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        //Коллекция книг
+        ConcurrentDictionary<string, int> bookCollection = new ConcurrentDictionary<string, int>();
+
+        // Поток для вычисления процентов
+        Task task = Task.Run(() => { PrecentBook(bookCollection); });
+        
+        //Главный цикл
+        var start = true;
+        do
+        {
+            Console.WriteLine("1 - добавить книгу|||2 - вывести список непрочитанного|||3 - выйти");
+
+            
+            var operB = int.TryParse(Console.ReadLine(), out int oper);
+            if (operB)
+            {
+                Console.Clear();
+                switch (oper)
+                {
+                    case 1:
+                        Console.WriteLine("Введите название книги:");
+                        AddBook(Console.ReadLine(), bookCollection);
+                        break;
+                    case 2:
+                        ShowBooks(bookCollection);
+                        break;
+                    case 3:
+                        start = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+           
+        } while (start);
+
+        
+    }
+
+    //Методы коллекции
+    public static void AddBook(string bookName, ConcurrentDictionary<string, int> bookCollection)
+    {
+        foreach (var item in bookCollection)
+        {
+            if (item.Key == bookName)
+            {
+                Console.WriteLine("Книга уже есть!");
+                break;
+            }
+        }
+        bookCollection.TryAdd(bookName, 0);
+    }
+
+    public static void ShowBooks(ConcurrentDictionary<string,int> bookCollection)
+    {
+        foreach (var item in bookCollection)
+        {
+            Console.WriteLine($"Название книги {item.Key} || Книга прочитана на {item.Value} процентов.");
+        }
+    }
+
+    //Метод расчёта процентов
+        public static void PrecentBook(ConcurrentDictionary<string, int> bookCollection)
+    {
+        while (true)
+        {
+            foreach (var item in bookCollection)
+            {
+                Thread.Sleep(100);
+                bookCollection[item.Key] = item.Value+1;
+                if (item.Value == 100)
+                {
+                    if (bookCollection.TryRemove(item.Key, out int retrievedValue))
+                    {
+                        Console.WriteLine($"Книга {item.Key} удалена");
+                    }
+                }
+            }
+        }
+        
+    }
+
+}
